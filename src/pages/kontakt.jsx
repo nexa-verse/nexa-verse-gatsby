5d1c59520a9a4aad2a60e15a4f73ed2181746832
Radosław Łuckoś <luckos.r@hotmail.com>
2025-05-10 15:10:35 +0200

    contact test form

---
+++ src/pages/kontakt.jsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+const ContactPage = () => {
+  return (
+    <main >
+      <h1>Skontaktuj się z nami</h1>
+      <form name="Contact Form" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="Contact Form" />
+        <div>
+          <label>Your Email:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label>Message:</label>
+          <textarea name="message" />
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </main>
+  )
+}
+
+export default ContactPage
+
+export const Head = () => <title>Contact Page</title>
