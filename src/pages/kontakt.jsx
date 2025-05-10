3014394499ea4b2479691cfc06cbf9b3c44a8e56
Radosław Łuckoś <luckos.r@hotmail.com>
2025-05-10 15:26:32 +0200

    Update kontakt.jsx

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -4,8 +4,8 @@ const ContactPage = () => {
   return (
     <main >
       <h1>Skontaktuj się z nami</h1>
-      <form name="Contact Form" method="POST" netlify>
-        <input type="hidden" name="form-name" value="Contact Form" />
+      <form name="kontakt" method="POST" netlify>
+        <input type="hidden" name="form-name" value="kontakt" />
         <div>
           <label>Your Email:</label>
           <input type="email" name="email" />
